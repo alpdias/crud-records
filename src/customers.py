e77934c9854bcda2d06a19a4e45c7f6ff1eb437d
Paulo <al.pdias24@gmail.com>
2019-11-22 20:57:49 -0300

    add new var's to program

--- src/customers.py
+++ src/customers.py
@@ -3,11 +3,11 @@ import pymysql
 from time import sleep
 from datetime import date
 
-numeroID = int(input('ID'))
-empresa = str(input('Empresa'))
-cnpj = str(input('CNPJ'))
-tributacao = str(input('Tributação'))
-email = str(input('E-mail'))
-telefone = str(input('Telefone'))
-endereco = str(input('Endereço'))
-situacao = str(input('Situação'))
+numeroID = int(input('ID: '))
+empresa = str(input('Empresa: '))
+cnpj = str(input('CNPJ: '))
+tributacao = str(input('Tributação: '))
+email = str(input('E-mail: '))
+telefone = str(input('Telefone: '))
+endereco = str(input('Endereço: '))
+situacao = str(input('Situação: '))
