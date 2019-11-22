d6fe95a971415e42371f6fb3109c28eea638179b
Paulo <al.pdias24@gmail.com>
2019-11-21 22:38:52 -0300

    add lib to script

---
+++ src/customers.py
@@ -0,0 +1,13 @@
+import os
+import pymysql
+from time import sleep
+from datetime import date
+
+numeroId = int(input('ID'))
+empresa = str(input('Empresa'))
+cnpj = str(input('CNPJ'))
+tributacao = str(input('Tributação'))
+email = str(input('E-mail'))
+telefone = str(input('Telefone'))
+endereco = str(input('Endereço'))
+situacao = str(input('Situação'))
