a06a26adb3a7c7a82b98ced9150a64aca9848dea
Paulo <al.pdias24@gmail.com>
2019-11-21 22:45:34 -0300

    add lib mysql

--- src/customers.py
+++ src/customers.py
@@ -3,7 +3,7 @@ import pymysql
 from time import sleep
 from datetime import date
 
-numeroId = int(input('ID'))
+numeroID = int(input('ID'))
 empresa = str(input('Empresa'))
 cnpj = str(input('CNPJ'))
 tributacao = str(input('Tributação'))
