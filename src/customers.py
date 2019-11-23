5638e175d5a1f9565069d7279ab794564a223a2f
Paulo <al.pdias24@gmail.com>
2019-11-22 21:20:58 -0300

    create list index

--- src/customers.py
+++ src/customers.py
@@ -4,12 +4,14 @@ import pymysql
 from time import sleep
 from datetime import date
 # main program
+print('')
 print('customer base')
-
+print('')
 menu = ['customers', 'search', 'include', 'changes', 'export']
 for index, listItems in enumerate(menu):
     print(f'[{index}] {listItems}')
 
+'''
 numberID = int(input('ID: '))
 company = str(input('Company Name: '))
 register = str(input('Registration Number: '))
@@ -18,3 +20,5 @@ email = str(input('E-mail: '))
 telephone = str(input('Phone Number: '))
 address = str(input('Address: '))
 situation = str(input('Current Situation: '))
+''' 
+print('')
