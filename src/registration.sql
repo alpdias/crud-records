ce23bde1041dece6ea461c6951e63d63b5690eaa
Paulo <al.pdias24@gmail.com>
2019-11-24 14:19:16 -0300

    create first items table sql

--- src/registration.sql
+++ src/registration.sql
@@ -10,4 +10,7 @@ CREATE TABLE registration (
     situation,
     PRIMARY KEY (id)
 );
+INSERT INTO registration VALUES 
+(1,'Google','080.123.666-0','income','google@google.com','511-452-3425','California','active'),
+(2,'Apple','123.455.999-7','sales','apple@apple.com','455-444-1312','California','inactive');
 *\
