63c7108dd7a1f9cb754e8e1baee3f31439ea29dc
Paulo <al.pdias24@gmail.com>
2019-11-26 20:43:21 -0300

    created options selector

--- src/customers.py
+++ src/customers.py
@@ -21,22 +21,37 @@ print('customer base')
 print('')
 menu = ['customers', 'search', 'include', 'changes', 'export']
 for index, listItems in enumerate(menu):
-    print(f'[{index}] {listItems}')
-
+    print(f'[{index + 1}] {listItems}')
+print('[0] to quit')
+print('')
+selectOption = int(input('select an option: '))
 # customers
-cursor = returnConnection()
-result = cursor.execute('SELECT * FROM registration')
-for result in cursor:
-    print(result)
+if selectOption == 1:
+    print('')
+    print('list of registered records')
+    print('')
+    cursor = returnConnection()
+    result = cursor.execute('SELECT * FROM registration')
+    for result in cursor:
+        print(result)
+#search
 
 # include
-numberID = int(input('ID: '))
-company = str(input('Company Name: '))
-register = str(input('Registration Number: '))
-tax = str(input('Tax: '))
-email = str(input('E-mail: '))
-telephone = str(input('Phone Number: '))
-office = str(input('Office Address: '))
-situation = str(input('Current Situation: '))
+if selectOption == 3:
+    print('')
+    print('to add a new record')
+    print('')
+    numberID = int(input('ID: '))
+    company = str(input('Company Name: '))
+    register = str(input('Registration Number: '))
+    tax = str(input('Tax: '))
+    email = str(input('E-mail: '))
+    telephone = str(input('Phone Number: '))
+    office = str(input('Office Address: '))
+    situation = str(input('Current Situation: '))
+
+#changes
+
+#export
 
 print('')
