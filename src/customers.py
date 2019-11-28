76f4d8087c129b9d0dec0fae37814c510478617c
Paulo <al.pdias24@gmail.com>
2019-11-28 20:29:05 -0300

    table and structure reformulation

--- src/customers.py
+++ src/customers.py
@@ -5,12 +5,12 @@ from time import sleep
 from datetime import date
 
 # functions
-connection = pymysql.connect(host = 'localhost', database = 'customers', user = 'root', passwd = '')
+connection = pymysql.connect(host = 'localhost', database = 'people', user = 'root', passwd = '')
 cursor = connection.cursor()
 
 # main program
 print('')
-print('Records Registration CRUD')
+print('CRUD - Records List')
 print('')
 menu = ['records list', 'records search', 'add new record', 'change record', 'export table', 'import table']
 for index, listItems in enumerate(menu):
@@ -22,7 +22,7 @@ selectOption = int(input('select an option to perform: '))
 # records list
 if selectOption == 1:
     print('')
-    print('list of registered records')
+    print('records list')
     print('')
     nameTable = str(input('select table to view: '))
     print('')
@@ -37,16 +37,16 @@ if selectOption == 3:
     print('')
     print('fill in to add a new record')
     print('')
-    numberID = str(input('ID: '))
-    company = str(input('Company Name: '))
-    register = str(input('Registration Number: '))
-    tax = str(input('Tax: '))
-    email = str(input('E-mail: '))
-    telephone = str(input('Phone Number: '))
-    office = str(input('Office Address: '))
-    situation = str(input('Current Situation: '))
-    cursor.execute("INSERT INTO registration (id, company, register, tax, email, telephone, office, situation) \
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (numberID, company, register, tax, email, telephone, office, situation))
+    numberID = str(input('ID (empty to self fill):  '))
+    fullName = str(input('Full Name: '))
+    profession = str(input('Profession: '))
+    birth = str(input('Birth [yyyy/mm/dd]: '))
+    genre = str(input('Genre [M/F]: '))
+    bodyweight = str(input('Body Weight: '))
+    height = str(input('Height: '))
+    nationality = str(input('Nationality: '))
+    cursor.execute("INSERT INTO people (id, fullname, profession, birth, genre, bodyweight, height, nationality)   \
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (numberID, fullName, profession, birth, genre, bodyweight, height, nationality))
     connection.commit()
 
 # change record
